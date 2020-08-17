5d03ba8a26c22a232cbfeb0314f75d3a562a9c99
Karlygach Shaiykova <kshaiykova@gmail.com>
2020-08-17 01:13:59 -0700

    add homeworks about functions

---
+++ pluralize/scripts.js
@@ -0,0 +1,10 @@
+function pluralize (number,word) {
+    var plural = number + word;
+    return plural;
+    
+  }
+  
+  pluralize(8 , ' cats');
+  pluralize(10 , ' rats');
+  pluralize(4 , ' birds');
+  
---
+++ the_fortune_teller/scripts.js
@@ -0,0 +1,10 @@
+function tellFortune(kids,partner,city,job){
+
+    var output = 'You will be a ' + job + ' in ' + city + ' and married to ' + partner +' with '+ kids;
+    console.log(output);
+    return output;
+}
+
+tellFortune( 5 , 'Kaku' , 'Redmond' , 'Software Engineer');
+tellFortune( 2 , 'Katya' , 'Seattle' , 'Engineer');
+tellFortune( 3 , 'Maria' , 'Italy' , 'Software Engineer');
