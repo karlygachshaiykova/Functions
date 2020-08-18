53a1c8d60c8bac6c4fb5eff15d522da50fbe7811
Karlygach Shaiykova <kshaiykova@gmail.com>
2020-08-18 01:29:14 -0700

    add 'compare' project

---
+++ compare_two/scripts.js
@@ -0,0 +1,17 @@
+function comparison(numberOne , numberTwo){
+    if (numberOne > numberTwo){
+      return numberOne;
+    }
+  
+  
+   else if (numberTwo > numberOne) {
+    return numberTwo;
+  }
+   else {
+    console.log ('incorrect values');
+  
+  }
+  }
+  
+  comparison(4 , 7);
+  comparison( 2 , 2);
